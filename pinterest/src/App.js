a127b5a989b4987dcfd355f1b08b4e34d0518973
nohestmm <nohestmm@gmail.com>
2019-08-17 21:21:31 -0300

    second commit, styling pinterest

--- pinterest/src/App.js
+++ pinterest/src/App.js
@@ -4,13 +4,15 @@ import '../node_modules/@fortawesome/fontawesome-free/js/all';
 import '../node_modules/@fortawesome/fontawesome-free/css/all.css';
 
 const clientId = "092762aa2384f8aef21f266b06fd40cc4018c6c36fb4493858d3330ab9988e8a";
-const mainUrl = "https://api.unsplash.com/search/photos"
+const mainUrl = "https://api.unsplash.com/search/photos";
+const randomUrl = "https://api.unsplash.com/photos" 
 
 class App extends React.Component{
   constructor(props){
     super(props)
   this.state={
-    images:[]
+    images:[],
+    show: false
   }
     this.query = '';
     this.getQueryValue = this.getQueryValue.bind(this);
@@ -18,37 +20,72 @@ class App extends React.Component{
    
   }
 
+  showModal = () => {
+    
+    this.setState({ show: true });
+  };
 
+  hideModal = () => {
+    this.setState({ show: false });
+  };
+ 
 
-  searchImg(){
-   fetch(`${mainUrl}?query=${this.query}&client_id=${clientId}`)
+  getQueryValue(e){
+    this.query= e.target.value;
+
+   this.searchImg(this.query)
+  } 
+  
+  searchImg(query){
+   fetch(`${mainUrl}?per_page=1000&query=${query}&client_id=${clientId}`)
    .then(res=> {
      return res.json()
    }).then(resJson=> {
      console.log(resJson);
      this.setState({images:resJson.results})
    })
-
-
-  }
-
-  getQueryValue(e){
-    this.query= e.target.value;
   }
   showImages(){
-    return this.state.images.map(img => {
-      return <img src={img.urls.thumb} alt="" key={img.id}/>
+    
+        return this.state.images.map(el => {
+      return <div className="content-img" key={el.id} ><img src={el.urls.thumb} alt="" key={el.id} onClick= {this.showModal} /></div>
     })
   }
-
+  componentDidMount() {
+    
+    fetch(`${randomUrl}?per_page=1000&client_id=${clientId}`)
+    .then(res=>{
+      return res.json()
+  }).then(resJson=>{
+      console.log(resJson);
+      this.setState({
+        images: resJson
+      })
+  })
+}
 
   render(){
     return(
-<div>
-  <input type="text" onChange={this.getQueryValue}/>
-  <button onClick = {this.searchImg}>Buscar</button>
-  <div>{this.showImages()}</div>
-</div>
+<>
+<nav className="navbar">
+  <div className="content-icon"><i className="fab fa-pinterest" onClick={this.showModal}></i></div>
+    <div className="content-input">
+      <div><i className="fas fa-search"></i></div>
+      <input className="input-search" placeholder="Buscar" type="text" onKeyPress={this.getQueryValue}/>
+    </div>
+  {/* <div><button onClick = {this.searchImg} className= "button-search">Buscar</button></div> */}
+  <div>Inicio</div>
+  <div>Siguiendo</div>
+  <div><img src="" alt=""/></div>
+  <div>Nohemi</div>
+  </nav>
+<div className="show-img">{this.showImages()}</div>
+    
+    
+    
+
+
+</>
       )
   }
 }
